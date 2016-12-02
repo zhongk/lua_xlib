7cc5753a4b33e84aaec0eae13a93095c477241ac
zhongk <ohnisama@qq.com>
2016-12-02 16:50:25 +0800

    Create md5sum.lua

---
+++ examples/md5sum.lua
@@ -0,0 +1,11 @@
+require('digest')
+require('base64')
+
+if not arg[1] then
+  print(string.format('usage: lua %s filename', arg[0]))
+	os.exit()
+end
+local md5 = get_digest('md5')
+local text = assert(io.open(arg[1], 'r')):read('*a')
+local md5sum = base16.encode(md5(text))
+print(md5sum)
